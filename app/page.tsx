4e556813022fab534b7788d8e9a098289a30b177
Andrés Marín <andresfprivado@gmail.com>
2024-02-21 22:29:17 +0100

    Main done

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 // Components
 import { Header } from "./components/Header/Header";
-import { Home } from "./components/Home/Home";
+import { Main } from "./components/Main/Main";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Main />
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
       sm: '480px',
       md: '768px',
       lg: '976px',
-      xl: '1440px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
     colors: {
       'primary': '#2E2DDD',
@@ -19,6 +20,7 @@ const config: Config = {
       'light-yellow': '#F9F2A2',
       'orange': '#FF4900',
       'beige': '#FFEAD9',
+      'dark-blue': '#007BD0',
       'white': '#fff',
       'black': '#000'
     },
@@ -28,7 +30,7 @@ const config: Config = {
         DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
-        xl: '12rem',
+        xl: '4rem',
         '2xl': '6rem',
       },
     },
