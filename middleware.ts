b56b049baf087affec626f47585ead4b737792fb
Andrés Marín <andresfprivado@gmail.com>
2024-05-04 01:08:33 +0200

    Clerk Update

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
-import { authMiddleware } from "@clerk/nextjs";
-export default authMiddleware({
-  publicRoutes: ["/", "/products/:id"],
-  ignoredRoutes: ["/assets/img/(.*)", "/api/webhooks(.*)"],
-});
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+// import { authMiddleware } from "@clerk/nextjs";
+// export default authMiddleware({
+//   publicRoutes: ["/", "/products/:id"],
+//   ignoredRoutes: ["/assets/img/(.*)", "/api/webhooks(.*)"],
+// });
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
