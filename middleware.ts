bb4f7e3c94bae2dc6fd4f357156e8f05f7d65c6d
Andrés Marín <andresfprivado@gmail.com>
2024-05-04 01:21:49 +0200

    Revert "Clerk Update"
    This reverts commit b56b049baf087affec626f47585ead4b737792fb.

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+  publicRoutes: ["/", "/products/:id"],
+  ignoredRoutes: ["/assets/img/(.*)", "/api/webhooks(.*)"],
+});
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-// import { authMiddleware } from "@clerk/nextjs";
-// export default authMiddleware({
-//   publicRoutes: ["/", "/products/:id"],
-//   ignoredRoutes: ["/assets/img/(.*)", "/api/webhooks(.*)"],
-// });
-// export const config = {
-//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
