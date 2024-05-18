17af074e11634b7586ca9aa7c1a2714df09f93cd
Andrés Marín <andresfprivado@gmail.com>
2024-05-18 22:35:58 +0200

    Fix Deploy. Orders undefined.

--- app/(root)/profile/orders/page.tsx
+++ app/(root)/profile/orders/page.tsx
@@ -102,7 +102,7 @@ const Orders = async () => {
               </div>
             </div>
             <div className="w-full h-40 flex gap-8 p-8 items-center">
-              {order.items.map((item: OrderItem, key: number) => (
+              {order.items!.map((item: any, key: number) => (
                 <div key={key} className="relative">
                   <Image
                     src={item.picture}
@@ -121,85 +121,6 @@ const Orders = async () => {
             </div>
           </li>
         ))}
-        {/* <li className="w-full h-auto bg-zinc-100  rounded-b-xl">
-          <div className="w-full h-auto bg-white border border-black rounded-xl flex justify-between items-center pl-4 pr-3 py-3">
-            <ul className="h-full flex gap-8 items-center">
-              <li className="h-full flex gap-3 items-center">
-                <div className="size-16 aspect-square rounded-full bg-black flex-center p-4">
-                  <svg
-                    viewBox="0 0 32 32"
-                    fill="currentColor"
-                    className="size-full text-white">
-                    <path d="M13.29,6.29l-.29.3V2h6V6.59l-.29-.3a1,1,0,0,0-1.42,0L16,7.59l-1.29-1.3a1,1,0,0,0-1.42,0ZM26,2H21V9a1,1,0,0,1-.62.92A.84.84,0,0,1,20,10a1,1,0,0,1-.71-.29L18,8.41l-1.29,1.3a1,1,0,0,1-1.42,0L14,8.41l-1.29,1.3a1,1,0,0,1-1.09.21A1,1,0,0,1,11,9V2H6A4,4,0,0,0,2,6V26a4,4,0,0,0,4,4H16A10,10,0,1,1,30,16V6A4,4,0,0,0,26,2Zm4,20a8,8,0,1,1-8-8A8,8,0,0,1,30,22Zm-4.29-2.71a1,1,0,0,0-1.42,0L21,22.59l-1.29-1.3a1,1,0,0,0-1.42,1.42l2,2a1,1,0,0,0,1.42,0l4-4A1,1,0,0,0,25.71,19.29Z" />
-                  </svg>
-                </div>
-                <div className="flex flex-col">
-                  <span className="text-sm font-semibold">Status</span>
-                  <span>{orderDelivered.status}</span>
-                </div>
-              </li>
-              <li className="flex flex-col">
-                <span className="text-sm font-semibold">Total</span>
-                <span>{orderDelivered.total} €</span>
-              </li>
-              <li className="flex flex-col">
-                <span className="text-sm font-semibold">Placed At</span>
-                <span>{orderDelivered.placedAt}</span>
-              </li>
-              <li className="flex flex-col">
-                <span className="text-sm font-semibold">Shipped to</span>
-                <span>{orderDelivered.address.name}</span>
-              </li>
-            </ul>
-            <div className="h-full flex flex-col justify-between items-end gap-2">
-              <span className="text-zinc-400 text-sm">
-                Order Nº {orderDelivered.id}
-              </span>
-              <div className="flex gap-1">
-                <Button
-                  isIconOnly
-                  aria-label="Report Incident"
-                  className="rounded-lg bg-red-400">
-                  <svg
-                    className="size-5 text-white"
-                    fill="currentColor"
-                    viewBox="0 0 16 16">
-                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                  </svg>
-                </Button>
-                <Button
-                  isIconOnly
-                  aria-label="Inform"
-                  className="rounded-lg bg-black">
-                  <svg
-                    className="size-5 text-white"
-                    fill="currentColor"
-                    viewBox="0 0 16 16">
-                    <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z" />
-                  </svg>
-                </Button>
-              </div>
-            </div>
-          </div>
-          <div className="w-full h-40 flex gap-8 p-8 items-center">
-            {orderDelivered.products.map((product: any) => (
-              <div key={product.id} className="relative">
-                <Image
-                  src={product.picture}
-                  alt="Product Image"
-                  width={96}
-                  height={96}
-                  className="size-16 h-auto"
-                />
-                {product.quantity > 1 && (
-                  <span className="absolute size-6 rounded-full bg-black text-white text-sm flex-center outline outline-4 outline-zinc-100 bottom-0 right-0">
-                    {product.quantity}
-                  </span>
-                )}
-              </div>
-            ))}
-          </div>
-        </li> */}
       </ul>
     </div>
   );
